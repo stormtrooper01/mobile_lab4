4ad1b5622a1b10a2c8f448ff1599326070427f8f
Yelena <lena.skripnik.01@gmail.com>
2021-05-10 11:48:45 +0300

    Create main.dart

---
+++ Part_2/Parameters/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/' : (context) => MainScreen(),
+      '/second' : (context) => SecondScreen(),
+    },
+  ));
+}
+
+class ScreenArguments{
+  final String message;
+  ScreenArguments(this.message);
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Main page'),
+          backgroundColor: Colors.teal[200],
+      ),
+      body: Center(
+          child: RaisedButton(
+            color: Colors.teal[200],
+          onPressed: (){
+            Navigator.pushNamed(context, '/second',
+                arguments: ScreenArguments('May the force be with you!')
+            );
+            },
+            child: Text('Open second page'))),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Second page'),
+          backgroundColor: Colors.teal[200],
+      ),
+      body: Center(
+          child: RaisedButton(
+            color: Colors.teal[200],
+          onPressed: (){
+            Navigator.pop(context);
+            },
+            child: Text(args.message),
+          )
+      ),
+    );
+  }
+}
